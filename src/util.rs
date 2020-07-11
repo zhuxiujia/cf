22448ed41b730e089aaaabacdebb3773bc9adf9b
zhuxiujia <zhuxiujia@qq.com>
2020-07-12 06:36:05 +0800

    add util

--- src/util.rs
+++ src/util.rs
@@ -11,8 +11,16 @@ pub unsafe fn click_send_input(dx: u32, dy: u32) {
     let mi = input.u.mi_mut();
     mi.dx = 0;
     mi.dy = 0;
-    mi.dwFlags =  MOUSEEVENTF_MOVE  | MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP;
-    SendInput(3, &mut input, size_of::<INPUT>() as i32);
+    mi.dwFlags =   MOUSEEVENTF_LEFTDOWN;
+    SendInput(1, &mut input, size_of::<INPUT>() as i32);
+    sleep(Duration::from_millis(100));
+
+    let mut input = INPUT { type_: INPUT_MOUSE, u: INPUT_u::default() };
+    let mi = input.u.mi_mut();
+    mi.dx = 0;
+    mi.dy = 0;
+    mi.dwFlags =  MOUSEEVENTF_LEFTUP;
+    SendInput(1, &mut input, size_of::<INPUT>() as i32);
     sleep(Duration::from_millis(200));
 }
 
